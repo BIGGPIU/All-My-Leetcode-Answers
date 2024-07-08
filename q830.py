ae6b5bd87eb80432c99209b5b0c0e771946a0308
BIGGPIU <114438660+BIGGPIU@users.noreply.github.com>
2024-07-08 16:11:20 -0400

    q830 RT 86ms MEM 16.52 mb

--- q830.py
+++ q830.py
@@ -2,28 +2,23 @@ sample = "abbxxxxzzy"
 
 class Solution:
     def largeGroupPositions(self, s: str) -> list[list[int]]:
-        past = ""
-        startindex = ""
-        endindex = ""
-        leading = False
-        answerdict = []
-        logged = []
+        answer = []
+        skip = []
         for i in range(len(s)):
-            pointer = s[i]
-            try:
-                hold = s[i+1]
-            except:
-                hold == None
-            if hold == pointer:
-                leading = True
-            else:
-                leading = False
-            if leading == True:
-                logged.append(i)
-            pass
-        return logged
-
-            
+            x = 1
+            if i not in skip:
+                while True:
+                    try:
+                        if s[i+x] == s[i]:
+                            skip.append(i+x)
+                            x+=1
+                        else:
+                            break
+                    except:
+                        break
+                if x >= 3:
+                    answer.append([i,i+x-1])
+        return answer
 
 if __name__ == "__main__":
     hold = Solution.largeGroupPositions(None,sample)
